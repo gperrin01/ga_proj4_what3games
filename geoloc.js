8c00a6a3075935a001633d83a36a6c72c006f69e
Guillaume Perrin <gperrin01@gmail.com>
2015-07-08 16:16:47 +0100

    moving maps and geoloc on the same file. About to recode the promise to break down into more functions: getcurrentpos, convertintowords, etc

--- geoloc.js
+++ geoloc.js
@@ -1,24 +1,64 @@
 // GEOLOC WILL NOT WORK FROM A FILE - NEEDS TO BE A SERVER
+var geo = navigator.geolocation;
+var geocoder;
+var map;
 
-var geo = navigator.geolocation
 $(document).ready(function(){
-  // init_geo();
-  $('#test').on('click', init_geo_promise)
+  map_initialize();
+  $('#submit_location').on('submit', codeAddress);
+  $('#where_am_i').on('click', get_words_current_pos)
 })
 
-function init_geo_promise() {
+// Load map hardcoded with London when the page opens
+function map_initialize() {
+  console.log('initializing the map');
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(51.50722, -0.12750);
+  var mapOptions = {
+    zoom: 12,
+    center: latlng
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
 
-  var p1 = new Promise(function(resolve, reject) {
+// Set pin on the map based on user input
+function codeAddress() {
+  event.preventDefault();
+  var address = $('#address_input').val();
 
-    console.log('promise started');
+  // get a pin on the map with the address input
+  geocoder.geocode( {'address': address}, function(results, status) {
+    console.log('results', results);
+    console.log('status', status);
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      map.setZoom(16);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+
+// get W3W to return the words for the current position
+function get_words_current_pos() {
+
+  return new Promise(function(resolve, reject) {
+
+    // return your location using html5 native geolocation
     if(!!geo) {
       console.log('your brower supports geoloc');
-      var wpid = geo.watchPosition(resolve, reject, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
+      var wpid = geo.getCurrentPosition(resolve, reject, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
     } else {
       console.log("ERROR: Your Browser doesnt support the Geo Location API");
     }
   });
 
+  // once location is grabbed, get the 3 words from w3w
   p1.then(function(val) {
     console.log('promise finishd return position', val);
     // Convert position into W3W words
