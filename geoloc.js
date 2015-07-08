a6cceb0906b6049cfd208a2f379e7dc375293807
Guillaume Perrin <gperrin01@gmail.com>
2015-07-08 20:49:51 +0100

    finished refactoring on all map geoloc functions

--- geoloc.js
+++ geoloc.js
@@ -1,18 +1,27 @@
 // GEOLOC WILL NOT WORK FROM A FILE - NEEDS TO BE A SERVER
+
 var geo = navigator.geolocation;
 var geocoder;
 var map;
+
 var init_marker;
+var init_marker_icon = 'http://www.agiespana.es/_portal/_widgets/googlemaps/red_marker.png';
 var zoomInit = 13;
 var zoomShowLocation = 16;
 
+
 $(document).ready(function(){
   mapInitialize();
-  $('#submit_location').on('submit', codeAddressAndWords);
-  $('#where_am_i').on('click', get_words_current_pos)
+
+  // Event Listeners
+  $('#submit_location').on('submit', setMapToLocation);
+  $('#where_am_i').on('click', setMapToWhereAmI)
 })
 
-// Load map hardcoded with London when the page opens
+// ******************************************
+// On page load: map centered in London
+// ******************************************
+
 function mapInitialize() {
   console.log('initializing the map');
 
@@ -33,11 +42,12 @@ function mapInitialize() {
     position: latlng,
     animation: google.maps.Animation.DROP,
     draggable: true,
-    icon: 'http://www.agiespana.es/_portal/_widgets/googlemaps/red_marker.png',
+    icon: init_marker_icon,
     title: 'Move me around!'
   });
-
+  // **
   // listener to update location and 3 words when the marker is dragged
+  // **
   google.maps.event.addListener(init_marker, 'dragend', function(event){
     position = this.position.A + ', ' + this.position.F;
     displayThreeWords(position);
@@ -46,17 +56,18 @@ function mapInitialize() {
 
   // show the 3 words
   displayThreeWords(londonLat + ', ' + londonLong);
-
   // LATER add infowindow to the marker with 3 words and location
 }
 
+// ******************************************
+// On entering new address: show new pin on the map, center the map, show location, show 3 words
+// ******************************************
 
-// User wrote an address => pin on the map and 3 words
-function codeAddressAndWords() {
+function setMapToLocation() {
   event.preventDefault();
-  var address = $('#address_input').val();
 
   // get the coordinates of the location typed
+  var address = $('#address_input').val();
   geocoder.geocode( {'address': address}, function(results, status) {
     console.log('results', results);
     console.log('status', status);
@@ -64,48 +75,48 @@ function codeAddressAndWords() {
     if (status == google.maps.GeocoderStatus.OK) {
 
       var location = results[0].geometry.location;
-      // reposition the marker and center the map
+      // reposition marker + center map + show location + show words
       centerOnUpdatedMarker(location);
-      // fetch the 3 words
       displayThreeWords(location.A + ', ' + location.F);
+      displayLocation(location.A + ', ' + location.F)
 
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+    } else alert('Geocode was not successful for the following reason: ' + status);
   });
 }
 
+// ******************************************
+// On Refresh Current Location: update marker and map on location + show on page + get 3 words
+// ******************************************
 
-// update the map with current location + show on page + get W3W to return the words
-function get_words_current_pos() {
-  var p1 = new Promise(function(resolve, reject) {
-    // get location using html5 native geolocation
-    if(!!geo) {
-      console.log('your brower supports geoloc');
-      var wpid = geo.getCurrentPosition(resolve, reject, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
-    } else {
-      console.log("ERROR: Your Browser doesnt support the Geo Location API");
-    }
-  });
-
-  // once location is grabbed, update the map and the words
-  p1.then(function(val) {
-    // update the w3w
-    var position = val.coords.latitude + ', ' + val.coords.longitude;
-    displayThreeWords(position);
-
-    // reposition the marker and center the map
-    var location = new google.maps.LatLng(val.coords.latitude, val.coords.longitude)
-    centerOnUpdatedMarker(location);
-
-    // update text on page
-
-
-  }).catch(function() {
-    console.log('promise was rejected');
-  })
+function setMapToWhereAmI() {
+  // get location using html5 native geolocation and wait for success
+  if(!!geo) {
+    console.log('your brower supports geoloc');
+    var wpid = geo.getCurrentPosition(geoloc_success, geoloc_error, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
+  } else {
+    console.log("ERROR: Your Browser doesnt support the Geo Location API");
+  }
 }
 
+// the 2 callback functions from setMapToWhereAmI
+function geoloc_success(val){
+  // once location is grabbed: show 3 words + show location + updater marker + center map
+  var position = val.coords.latitude + ', ' + val.coords.longitude;
+  displayThreeWords(position);
+  displayLocation(position);
+
+  var location = new google.maps.LatLng(val.coords.latitude, val.coords.longitude)
+  centerOnUpdatedMarker(location);
+}
+function geoloc_error(val){
+  console.log('could not get your current location');
+}
+
+// ******************************************
+// Functions to update the look on the page
+// ******************************************
+
+// Update marker position to new location and center the map and ensure zoon close
 function centerOnUpdatedMarker(location) {
   map.setCenter(location);
   map.setZoom(zoomShowLocation);
@@ -114,7 +125,7 @@ function centerOnUpdatedMarker(location) {
 
 // Display the location (based on coordinates) on the input box
 function displayLocation(location) {
-  $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position, 
+  $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + location, 
     function(result) {
       var text = result.results[0].address_components[0].long_name + ' ' + result.results[0].address_components[1].long_name;
       $('#address_input').val(text);
@@ -129,7 +140,6 @@ function displayThreeWords(position){
     'position': position,
     'lang': 'en'
   };
-
   $.get("https://api.what3words.com/position", data, function(response){
     console.log(response.words.join(' '));
     $('#3_words_list').text('Your 3 words: ' + response.words.join(' '));
