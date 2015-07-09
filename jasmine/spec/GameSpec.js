85518e3ac06199ac1d63d9455c0907b086279aac
Guillaume Perrin <gperrin01@gmail.com>
2015-07-09 16:45:13 +0100

    writing rules of the game before test cases

---
+++ jasmine/spec/GameSpec.js
@@ -0,0 +1,6 @@
+// SPECS
+// 1 - Word must be a real word, checked by an API
+// Word must be >= 2 letters
+// 2 - Word cannot use a letter which is not in the 3 words
+// 3 - Word cannot be any of the 3 words, nor the singular of them
+// 4 - Count of letter in the Word cannot be higher than cumulated count of same letter in the 3 words
--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -4,11 +4,6 @@
 //   $('#submit_answer').on('click', submitAnswer)
 // })
 
-// SPECS
-// 1 - Word must be a real world, checked by an API
-// 2 - Word cannot use a letter which is not in the 3 words
-// 3 - Word cannot be any of the 3 words, nor the singular of them
-// 4 - Count of letter in the Word cannot be higher than cumulated count of same letter in the 3 words
 
 
 var Word = Word || {};
