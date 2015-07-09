7653ad429efa838d2e8b45fe803ad29ca4c31691
Guillaume Perrin <gperrin01@gmail.com>
2015-07-09 16:41:26 +0100

    writing the rules of the game before going on with the test cases

--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -4,7 +4,19 @@
 //   $('#submit_answer').on('click', submitAnswer)
 // })
 
-// 
+// SPECS
+// 1 - Word must be a real world, checked by an API
+// 2 - Word cannot use a letter which is not in the 3 words
+// 3 - Word cannot be any of the 3 words, nor the singular of them
+// 4 - Count of letter in the Word cannot be higher than cumulated count of same letter in the 3 words
+
+
+var Word = Word || {};
+
+Word = {
+
+  // check if word is a real world (need API)
+}
 
 function submitAnswer(){
   event.preventDefault();
@@ -18,3 +30,25 @@ function submitAnswer(){
   console.log(this);
   console.log('submitting answer');
 }
+
+function isValid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
