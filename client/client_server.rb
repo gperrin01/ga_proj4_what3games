7cab895c6fabc9b9bd38eb534a5dc73b99ef73e4
Guillaume Perrin <gperrin01@gmail.com>
2015-07-28 14:20:30 +0100

    new syntax for sinatra to serve an html file, finally ok to render my page with foreman (used to be ok with sinatra but not foreman, now seems ok with both)

--- client/client_server.rb
+++ client/client_server.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
+# set :public_folder, '/client'
+
 get '/' do 
-  send_file File.join(settings.public_folder, 'index.html')
+  # send_file File.join(settings.public_folder, 'index.html')
+  File.read(File.join('client/public', 'index.html'))
 end  
